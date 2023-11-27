215377933ba473b77ceeb9628628fcf26a2f2155
Huanle01 <huanle120201@gmail.com>
2023-11-27 22:30:18 +0700

    set up cors

--- src/server.js
+++ src/server.js
@@ -1,15 +1,18 @@
 /* eslint-disable no-console */
 import express from "express";
 import exitHook from "async-exit-hook";
+import cors from "cors";
 import { CONNECT_DB, CLOSE_DB } from "~/config/mongodb";
 import { env } from "~/config/environment";
 import { APIs_V1 } from "~/routes/v1";
 import { errorHandlingMiddleware } from "./middlewares/errorHandlingMiddleware";
+import { corsOptions } from "./config/cors";
 const START_SERVER = () => {
   const app = express();
-
+  
   const hostname = env.APP_HOST;
   const port = env.APP_PORT || 8080;
+  app.use(cors(corsOptions))
   app.use(express.json());
   app.use("/v1", APIs_V1);
   app.use(errorHandlingMiddleware);
