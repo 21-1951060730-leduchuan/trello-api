a3a15cdc9471e8d5fa5219a2aed6f25b33408cc6
Huanle01 <huanle120201@gmail.com>
2023-12-12 23:22:04 +0700

    render deploy

--- src/server.js
+++ src/server.js
@@ -13,14 +13,21 @@ const START_SERVER = () => {
   const hostname = env.APP_HOST;
   const port = env.APP_PORT || 8080;
   // app.use(cors(corsOptions))
-  app.use(cors(corsOptions))
+  app.use(cors(corsOptions));
   app.use(express.json());
   app.use("/v1", APIs_V1);
   app.use(errorHandlingMiddleware);
-  app.listen(port, hostname, () => {
-    // eslint-disable-next-line no-console
-    console.log(`Hello ,I am running at ${hostname}:${port}`);
-  });
+  if (env.BUILD_MODE === "prod") {
+    app.listen(process.env.PORT, () => {
+      // eslint-disable-next-line no-console
+      console.log(`Hello ,I am running at ${process.env.PORT}`);
+    });
+  } else {
+    app.listen(port, hostname, () => {
+      // eslint-disable-next-line no-console
+      console.log(`Hello ,I am running at ${hostname}:${port}`);
+    });
+  }
   exitHook(() => {
     CLOSE_DB();
   });
